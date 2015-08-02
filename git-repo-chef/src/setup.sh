7397cdb0f5d44d99f0394a13f983ece43c62b41e
littleonikko <little.my.oniko@gmail.com>
2015-08-03 00:31:28 +0900

    Update setup.sh
    Add mkdir /etc/chef and solo.rb

--- git-repo-chef/src/setup.sh
+++ git-repo-chef/src/setup.sh
@@ -2,5 +2,8 @@
 yum update -y
 yum install -y ruby ruby-devel rubygems make gcc
 gem install chef
-mkdir -p /home/ec2-user/cookbooks
+mkdir /home/ec2-user/cookbooks
 chown -R ec2-user:ec2-user /home/ec2-user/cookbooks
+mkdir -p /etc/chef
+chmod 666 /etc/chef
+echo 'cookbook_path ["/home/ec2-user/chef-repo/cookbooks"]' > /etc/chef/solo.rb
